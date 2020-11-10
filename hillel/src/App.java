464b59a8bc20678dced1f59704406625c71ddafb
Viacheslav Soloviov <vsolovyev@provectus.com>
2020-11-11 00:03:21 +0200

    Home task #2

---
+++ hillel/src/App.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class App {
+    Scanner scanner = new Scanner(System.in);
+
+    public void averageSumOfArr(){
+        System.out.println("Provide total n. of elements");
+        int size = scanner.nextInt();
+        if (size < 2){
+            throw new IllegalArgumentException("Size of Arr should be grater or equal to 2");
+        }
+        int summ = 0;
+        float average;
+        int[] arr = new int[size];
+        System.out.println("Provide numbers to calculate average value");
+        for (int i=0; i<size; i++){
+            arr[i] +=scanner.nextInt();
+            summ+=arr[i];
+        }
+        average = (float) summ/size;
+        System.out.println("Avarege is: " + average);
+    }
+    public void averageSumOfTwo(){
+        int size = 2;
+        int[] arr = new int[size];
+        int summ = 0;
+        float average;
+        System.out.println("Provide numbers to calculate average value");
+        for (int i=0;i<size; i++){
+            arr[i]+=scanner.nextInt();
+            summ+=arr[i];
+        }
+        average =(float) summ/size;
+        System.out.println("Avarege is: " + average);
+    }
+    public void deposit() {
+        System.out.println("Provide amount of deposit");
+        double amount = scanner.nextDouble();
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount should not be negative");
+        }
+        System.out.println("Provide annual percentage");
+        double percentage = scanner.nextDouble()/100;
+        if (percentage < 0) {
+            throw new IllegalArgumentException("Percentage should be grater than 0");
+        }
+        System.out.println("Provide total years of deposit");
+        int years = scanner.nextInt();
+        if (years < 0) {
+            throw new IllegalArgumentException();
+        }
+        double percentageAmountPerYear = amount * percentage;
+        for (int i = 1; i < years+1; i++){
+            System.out.printf("%s; ", "Amount in year "+ i +": " + (amount + (percentageAmountPerYear*i)));
+            System.out.printf("%s %n","Year percentage: " + (percentageAmountPerYear*i));
+        }
+    }
+
+    public static void main(String[] args) {
+        App app =new App();
+        app.averageSumOfArr();
+        app.averageSumOfTwo();
+        app.deposit();
+    }
+}
